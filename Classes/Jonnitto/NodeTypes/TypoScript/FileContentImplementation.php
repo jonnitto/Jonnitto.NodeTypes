71abf9633477cc40ce086bc1b11e8e46c7c947dd
Jon Uhlmann <me@jonnitto.ch>
2016-12-19 20:50:49 +0100

    Inline SVG

---
+++ Classes/Jonnitto/NodeTypes/TypoScript/FileContentImplementation.php
@@ -0,0 +1,55 @@
+<?php
+namespace Jonnitto\NodeTypes\TypoScript;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TypoScript\TypoScriptObjects\AbstractTypoScriptObject;
+use TYPO3\Flow\Utility;
+
+
+class FileContentImplementation extends AbstractTypoScriptObject
+{
+
+    /**
+     * The location of the resource, must be a resource://... URI
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->tsValue('path');
+    }
+
+    /**
+     * If specified, this resource object is used instead of the path and package information
+     *
+     * @return Resource
+     */
+    public function getResource()
+    {
+        return $this->tsValue('resource');
+    }
+
+    /**
+     * Returns the file content of a resource
+     *
+     * @return string
+     * @throws TypoScriptException
+     */
+    public function evaluate()
+    {
+        $resource = $this->getResource();
+
+        if ($resource) {
+            return stream_get_contents($resource->getStream());
+        }
+
+        $path = $this->getPath();
+        if ($path) {
+            return Utility\Files::getFileContents($path);
+        }
+
+        return false;
+    }
+}
+
+//Utility\Files::getFileContents('resource://Blaser.Elements/Resources/Public/Assets/Logo.svg');
